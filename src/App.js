4e58022267eee9909a0d2fe45e3b6cb0103d9fd6
Alex Korjeski <akorjeski@gmail.com>
2020-08-18 16:49:47 -0400

    initial commit, added dragonite photo and some text

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import dragonite from './dragonite.gif'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={dragonite} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Alex is building his own e-Portfolio. Stay tuned!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
