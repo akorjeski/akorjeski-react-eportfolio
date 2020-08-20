327c9f8ba9871c4a27d3614840af49909e7f5845
Alex Korjeski <akorjeski@gmail.com>
2020-08-20 17:49:31 -0400

    links now go places!

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -27,21 +27,21 @@ function Home() {
   
   <Row>
 
-<Col className="myphoto"><img src={githubicon} alt='githubicon' />
+<Col className="myphoto"> <a href='https://github.com/akorjeski'><img src={githubicon} alt='githubicon' /> </a>
     <h4> Github </h4>
     
     </Col>
-    <Col className="myphoto"><img src={linkedinicon} alt='githubicon' />
+    <Col className="myphoto"> <a href='https://www.linkedin.com/in/alexander--korjeski/'><img src={linkedinicon} alt='linkedinicon' /></a>
     <h4> LinkedIn </h4>
     
     </Col>
     </Row>
     <Row>
-    <Col className="myphoto"><img src={resumeicon} alt='githubicon' />
+    <Col className="myphoto"> <a href='../../myresume.pdf'><img src={resumeicon} alt='resumeicon' /> </a>
     <h4> My Resume </h4>
     
     </Col>
-    <Col className="myphoto"><img src={portfolioicon} alt='githubicon' />
+    <Col className="myphoto"><img src={portfolioicon} alt='portfolioicon' />
     <h4> My Portfolio </h4>
     
     </Col>
