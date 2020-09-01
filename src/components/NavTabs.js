21846f1e12b6c15920dbd62def7f2415634c1451
Alex Korjeski <akorjeski@gmail.com>
2020-09-01 10:41:12 -0400

    updated the menu to be a cooler button

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -13,14 +13,16 @@ function NavTabs() {
   return (
 
     <Dropdown>
-      <Dropdown.Toggle variant="success" id="dropdown-basic">
-        Dropdown Button
+      <Dropdown.Toggle variant="dark" id="dropdown-basic">
+      <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-justify" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+  <path fill-rule="evenodd" d="M2 12.5a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5zm0-3a.5.5 0 0 1 .5-.5h11a.5.5 0 0 1 0 1h-11a.5.5 0 0 1-.5-.5z"/>
+</svg>
   </Dropdown.Toggle>
 
       <Dropdown.Menu>
         <Dropdown.Item ><Link to="/" className={location.pathname === "/" ? "nav-link active" : "nav-link"}>Home</Link>
 </Dropdown.Item>
-<Dropdown.Item ><Link to="/about" className={location.pathname === "/about" ? "nav-link active" : "nav-link"}>Aboot</Link>
+<Dropdown.Item ><Link to="/about" className={location.pathname === "/about" ? "nav-link active" : "nav-link"}>About</Link>
 </Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
